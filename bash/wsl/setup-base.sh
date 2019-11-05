507fe51e73bc4b182f83d8328724ea7c02af3650
Jim Andreasen <jim.andreasen@reifnir.com>
2019-11-05 18:01:44 -0500

    About to move to WSL 2

--- bash/wsl/setup-base.sh
+++ bash/wsl/setup-base.sh
@@ -20,7 +20,10 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 echo "Adding Docker CE CentOS repo..."
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Uninstall legacy versions of Docker
+echo "Adding CentOS 7 IUS repo..."
+sudo dnf install  https://centos7.iuscommunity.org/ius-release.rpm -y
+
+# Uninstall legacy versions of Docker and Git... we'll pull from other repos
 sudo dnf remove \
     docker \
     docker-client \
@@ -29,11 +32,14 @@ sudo dnf remove \
     docker-latest \
     docker-latest-logrotate \
     docker-logrotate \
-    docker-engine
+    docker-engine \
+    git \
+    -y
 
 # Expects to be run on a CentOS 7 machine (until I check out Pengwin next month)
 echo "Install all the things..."
 sudo dnf install \
+    git2u-all \
     azure-cli \
     python3 \
     kubectl \
@@ -50,19 +56,8 @@ pip3 install awscli --upgrade --user
 echo "Install Helm..."
 curl https://git.io/get_helm.sh -L | sh
 
-echo "Connect Docker CLI to Windows docker daemon port"
-# First clear any previous statement that begins with 'export DOCKER_HOST=' from ~/.bashrc
-sed -i "/export DOCKER_HOST=/d" ~/.bashrc
-echo "export DOCKER_HOST=tcp://localhost:23750" >> ~/.bashrc && source ~/.bashrc
-export DOCKER_HOST=tcp://localhost:23750
-
 echo "Grant plebs like us access to the Docker CLI without sudo..."
 sudo usermod -aG docker $USER
-
-# DOCKER HELP!: I keep seeing: ERROR: Cannot connect to the Docker daemon at tcp://localhost:23750. Is the docker daemon running?
-# (thanks to this: https://forums.docker.com/t/wsl-and-docker-for-windows-cannot-connect-to-the-docker-daemon-at-tcp-localhost-2375-is-the-docker-daemon-running/63571/13)
-# Run the following FROM WINDOWS once and you're perpetually GTG
-# docker run -d --restart=always -p 127.0.0.1:23750:2375 -v /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
 
 echo "Upgrade all RPM packages..."
 sudo dnf updateinfo
@@ -70,4 +65,3 @@ sudo dnf check-update
 sudo dnf upgrade -y
 
 echo "Make soft links to my Windows profile directories..."
-mklink
