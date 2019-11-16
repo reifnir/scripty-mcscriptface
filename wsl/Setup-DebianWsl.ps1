12fac19fa2455e6365d103c80a1d8910d2c4489a
Jim Andreasen <jim.andreasen@reifnir.com>
2019-11-16 15:36:29 -0500

    sets up the distro initialization now

--- wsl/Setup-DebianWsl.ps1
+++ wsl/Setup-DebianWsl.ps1
@@ -4,3 +4,6 @@ $DISTRO_LOCATION="https://aka.ms/wsl-debian-gnulinux"
 Invoke-WebRequest -Uri $DISTRO_LOCATION -OutFile .\distro.appx -UseBasicParsing
 
 Add-AppxPackage .\distro.appx
+
+# Can't call wsl until you kick off the distro setup
+debian
