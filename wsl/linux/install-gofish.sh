d8ea797e338a1ffea05d4372024bf21a3d0248a2
Jim Andreasen <jandreasen@kbra.com>
2019-11-19 20:08:40 -0500

    with helm 2 vs 3 changes, not adding by default

--- wsl/linux/install-gofish.sh
+++
@@ -1,9 +0,0 @@
-# Install GoFish
-curl -fsSL https://raw.githubusercontent.com/fishworks/gofish/master/scripts/install.sh | bash
-
-# Initialize
-gofish init
-
-# Upgrade GoFish to latest
-gofish install gofish
-gofish upgrade gofish
